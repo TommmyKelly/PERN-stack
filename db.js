913a04262f937ba64606e3537d1789269b4d5ab6
TommmyKelly <tommy_kelly@icloud.com>
2021-08-13 13:57:46 +0100

    initial commit

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  database: "pern",
-  user: "postgres",
-  host: "localhost",
-  password: "9630*lie",
-  port: 5432,
+  database: process.env.database,
+  user: process.env.user,
+  host: process.env.host,
+  password: process.env.password,
+  port: process.env.port,
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const pool = require("./db");
+require("dotenv").config();
 
 app.use(express.json());
 
